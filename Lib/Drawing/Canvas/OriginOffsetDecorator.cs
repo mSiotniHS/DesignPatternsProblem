bc760a879f7bd2a0defa88a329842e7f561f0f76
Artyom Abramyan <artemabramyan7@icloud.com>
2023-10-30 11:47:05 +0300

    Добавил декоратор, смещающий рисунок на заданное смещение

---
+++ Lib/Drawing/Canvas/OriginOffsetDecorator.cs
@@ -0,0 +1,25 @@
+﻿namespace Lib.Drawing.Canvas;
+
+public class OriginOffsetDecorator : ICanvasAdapter
+{
+	private readonly ICanvasAdapter _adapter;
+	private readonly Point _offset;
+
+	public OriginOffsetDecorator(ICanvasAdapter adapter, Point offset)
+	{
+		_adapter = adapter;
+		_offset = offset;
+	}
+
+	public void DrawLine(Point start, Point end) =>
+		_adapter.DrawLine(AddOffset(start), AddOffset(end));
+
+	public void DrawText(string value, double size, Point topLeft) =>
+		_adapter.DrawText(value, size, AddOffset(topLeft));
+
+	public Size MeasureTextSize(string text, double size) =>
+		_adapter.MeasureTextSize(text, size);
+
+	private Point AddOffset(Point point) =>
+		new(_offset.X + point.X, _offset.Y + point.Y);
+}

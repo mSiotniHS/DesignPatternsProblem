46bb2d5a463da3b8c1ea0290e50ba9cafa4e66e3
Artyom Abramyan <artemabramyan7@icloud.com>
2023-10-30 11:48:21 +0300

    Инициализировал проект приложения

---
+++ App/Program.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using System;
+
+namespace App;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args) =>
+	    BuildAvaloniaApp()
+		    .StartWithClassicDesktopLifetime(args);
+
+    private static AppBuilder BuildAvaloniaApp() =>
+	    AppBuilder.Configure<App>()
+			.UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}

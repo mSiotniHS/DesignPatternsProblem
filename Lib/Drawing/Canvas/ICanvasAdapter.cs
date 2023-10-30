5f082fdf66790e27e70a16dcf608aa5e718c281a
Artyom Abramyan <artemabramyan7@icloud.com>
2023-10-30 11:46:21 +0300

    Добавил ICanvasAdapter
    Описывает нечто, где можно рисовать примитивы

---
+++ Lib/Drawing/Canvas/ICanvasAdapter.cs
@@ -0,0 +1,11 @@
+namespace Lib.Drawing.Canvas;
+
+public record struct Point(double X, double Y);
+public record struct Size(double Width, double Height);
+
+public interface ICanvasAdapter
+{
+    void DrawLine(Point start, Point end);
+    void DrawText(string value, double size, Point topLeft);
+    Size MeasureTextSize(string text, double size);
+}

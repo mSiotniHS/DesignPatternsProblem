5f5bd1815b9e6a8080dd2215dd078a056b306a04
Artyom Abramyan <artemabramyan7@icloud.com>
2023-10-31 23:31:59 +0300

    Обновил ITextAdapter
    Теперь располагает текст в соответствии с номером строки и столбца

--- Lib/Drawing/Text/ITextAdapter.cs
+++ Lib/Drawing/Text/ITextAdapter.cs
@@ -1,8 +1,9 @@
 namespace Lib.Drawing.Text;
 
+public record struct PointerPosition(int Line, int Column);
+
 public interface ITextAdapter
 {
-    void Write(string value);
-    void Write(char value);
-    void Clear();
+    void Write(string value, PointerPosition startingPosition);
+    void Write(char value, PointerPosition position);
 }

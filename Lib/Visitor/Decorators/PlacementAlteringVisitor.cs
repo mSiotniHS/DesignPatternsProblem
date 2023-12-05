9f0739df7f2fc6197cc87e6281ea19bd4eeb3756
Artyom Abramyan <artemabramyan7@icloud.com>
2023-12-05 23:38:22 +0300

    Add `PlacementAlteringVisitor`

---
+++ Lib/Visitor/Decorators/PlacementAlteringVisitor.cs
@@ -0,0 +1,27 @@
+﻿namespace Lib.Visitor.Decorators;
+
+public class PlacementAlteringVisitor : IElementVisitor
+{
+    public delegate (uint, uint) CoordinateAlternator(uint row, uint column);
+
+    private readonly IElementVisitor _visitor;
+    private readonly CoordinateAlternator _alternator;
+
+    public PlacementAlteringVisitor(IElementVisitor visitor, CoordinateAlternator alternator)
+    {
+        _visitor = visitor;
+        _alternator = alternator;
+    }
+
+    public void VisitElement(uint row, uint column, double element)
+    {
+        var (alteredRow, alteredColumn) = _alternator(row, column);
+        _visitor.VisitElement(alteredRow, alteredColumn, element);
+    }
+
+    public void VisitSparseElement(uint row, uint column, double element)
+    {
+        var (alteredRow, alteredColumn) = _alternator(row, column);
+        _visitor.VisitSparseElement(alteredRow, alteredColumn, element);
+    }
+}
